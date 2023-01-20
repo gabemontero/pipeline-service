d1ba7dcd3680869bfa040958cb56f8235dd0ac8f
gabemontero <gmontero@redhat.com>
2023-01-20 10:58:39 -0500

    dump minio subscription for potential debug

--- operator/images/cluster-setup/content/bin/install.sh
+++ operator/images/cluster-setup/content/bin/install.sh
@@ -167,6 +167,9 @@ install_minio() {
 
   check_deployments "openshift-operators" "minio-operator" | indent 2
 
+  echo -n "- Display Minio Subscription information for potential debug: "
+  kubectl -n openshift-operators get subscriptions minio-operator -o yaml
+
   echo -n "- Minio tenant: "
   kubectl apply -k "$DEV_DIR/operators/$APP/tenant" >/dev/null
   echo "OK"
